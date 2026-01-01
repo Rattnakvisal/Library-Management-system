34956835c4999bc8989787c33c2b1dc700cdfc5e
jumnert <chountheachumnith@gmail.com>
2026-01-01 14:32:27 +0700

    Feature: Add navbar and footer

--- wwwroot/js/users/navbar.js
+++ wwwroot/js/users/navbar.js
@@ -1,1 +1,11 @@
-﻿
+﻿window.addEventListener('load', function () {
+    var header = document.querySelector('header');
+
+    document.addEventListener('scroll', function () {
+        if (window.pageYOffset > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
