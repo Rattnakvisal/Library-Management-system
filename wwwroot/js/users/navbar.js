89fa418f9dd96078eb06302be661405622d878f7
jumnert <chountheachumnith@gmail.com>
2026-01-02 19:13:33 +0700

    Feature: Add responsiveness to nav and footer

--- wwwroot/js/users/navbar.js
+++ wwwroot/js/users/navbar.js
@@ -1,11 +1,30 @@
-﻿window.addEventListener('load', function () {
-    var header = document.querySelector('header');
+﻿document.addEventListener('DOMContentLoaded', function () {
 
-    document.addEventListener('scroll', function () {
-        if (window.pageYOffset > 50) {
+   
+    const header = document.querySelector('header');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
         }
     });
+
+    const toggleBtn = document.querySelector('.mobile-menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    const icon = toggleBtn.querySelector('i');
+
+    if (toggleBtn) {
+        toggleBtn.addEventListener('click', function () {
+            navLinks.classList.toggle('active');
+            if (navLinks.classList.contains('active')) {
+                icon.classList.remove('bi-list');
+                icon.classList.add('bi-x-lg');
+            } else {
+                icon.classList.remove('bi-x-lg');
+                icon.classList.add('bi-list');
+            }
+        });
+    }
 });
