88b9cb971a6a51bf4831d4a3b668a034da8a0dc0
Lay Hongly <honglylay14@gmail.com>
2026-01-02 15:19:28 +0700

    add an admin layout and make the sidebar

---
+++ Controllers/Admin/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_system.Controllers.Admin
+{
+    [Route("admin/dashboard")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Dashboard/Index.cshtml");
+        }
+    }
+}
