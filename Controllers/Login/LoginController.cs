eb08c1ff162e2271368c40e3d5b330359da839dd
Namchheav009 <namchheavron.info@gmail.com>
2026-01-03 11:08:35 +0700

    chheav-commit login

---
+++ Controllers/Login/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_system.Controllers.Login
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
