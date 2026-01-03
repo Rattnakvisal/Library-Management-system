570a1e9ec7349aa76729884a5e31fac5ca93b991
Lay Hongly <honglylay14@gmail.com>
2026-01-03 13:45:04 +0700

    change admin controllers

---
+++ Controllers/Admin/ManageBooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_system.Controllers.Admin;
+
+public class ManageBooksController : Controller
+{
+    // GET
+    [Route("admin/managebooks")]
+    public IActionResult Index()
+    {
+        return View("~/Views/Admin/ManageBooks/ManageBooks.cshtml");
+    }
+}
---
+++ Controllers/Admin/ManageReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_system.Controllers.Admin;
+
+public class ManageReportController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
