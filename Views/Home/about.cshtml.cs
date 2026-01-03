b8475a870576089c8ac06e3418841128e2d74a19
Jumnert <chountheachumnith@gmail.com>
2026-01-03 14:55:18 +0700

    Feature: Add about page

---
+++ Views/Home/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library_Management_system.Views.Home
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
