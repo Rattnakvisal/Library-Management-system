66418fbab6a9d89ea474c37064193b820c9271a3
Lay Hongly <honglylay14@gmail.com>
2026-01-04 17:49:03 +0700

    complete the styling of admin manage book

---
+++ wwwroot/js/admin/Managebooks.js
@@ -0,0 +1,36 @@
+﻿const search_input = document.querySelector('.search-input');
+const search_icon = document.querySelector('.search-icon');
+
+search_input.addEventListener('focus', () => {
+    search_icon.style.display = 'none';
+});
+
+search_input.addEventListener('blur', () => {
+    if(search_input.value === '') {
+        search_icon.style.display = 'inline';
+    }
+});
+
+
+// Select all status spans
+const bookStatuses = document.querySelectorAll('.table-status');
+
+// Loop through each one
+bookStatuses.forEach(status => {
+    if (status.innerText === 'Available') {
+        status.style.backgroundColor = '#DBFCE7';
+        status.style.border = '1px solid #7BF1A6';
+        status.style.display = 'flex';
+        status.style.justifyContent = 'center';
+        status.style.borderRadius = '10px';
+        status.style.padding = '2px 6px'; // optional: spacing
+    }
+    else if (status.innerText === 'Unavailable') {
+        status.style.backgroundColor = '#FEA8A9';
+        status.style.border = '1px solid #FF0A0A';
+        status.style.display = 'flex';
+        status.style.justifyContent = 'center';
+        status.style.borderRadius = '10px';
+        status.style.padding = '2px 6px';
+    }
+});
