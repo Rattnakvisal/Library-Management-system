a273bb9a24d98b6200a7fa9d8c280fcb9e245cde
Lay Hongly <honglylay14@gmail.com>
2026-01-16 16:31:34 +0700

    complete admin feedback

--- wwwroot/js/admin/Feedback.js
+++ wwwroot/js/admin/Feedback.js
@@ -1,1 +1,32 @@
-﻿
+﻿$(document).ready(function() {
+    initializeSearchIcon();
+});
+
+function initializeSearchIcon() {
+    const search_feedback_input = $('#search-feedback-input');
+    const search_feedback_icon = $('#search-feedback-icon');
+    const book_review_input = $('#book-review-input');
+    const book_review_icon = $('#book-review-icon');
+
+    // Borrowing search
+    search_feedback_input.on('focus', function () {
+        search_feedback_icon.hide();
+    });
+
+    search_feedback_input.on('blur', function () {
+        if (search_feedback_input.val().trim() === '') {
+            search_feedback_icon.show();
+        }
+    });
+
+    // Reservation search
+    book_review_input.on('focus', function () {
+        book_review_icon.hide();
+    });
+
+    book_review_input.on('blur', function () {
+        if (book_review_input.val().trim() === '') {
+            book_review_icon.show();
+        }
+    });
+}
