1cb1f1db918b633a5e9a8a25ef5d523e3748bf7f
Rattnakvisal <rattnakvisalchun.com>
2026-01-17 14:25:23 +0700

    Fix Style max-width admin page

--- wwwroot/js/admin/dashboard.js
+++ wwwroot/js/admin/dashboard.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".qtab");
+  const contents = document.querySelectorAll(".qtab-content");
+
+  tabs.forEach((t) => {
+    t.addEventListener("click", () => {
+      tabs.forEach((x) => x.classList.remove("active"));
+      t.classList.add("active");
+
+      const target = t.getAttribute("data-target");
+      contents.forEach((c) => c.classList.remove("show"));
+      document.querySelector(target)?.classList.add("show");
+    });
+  });
+});
