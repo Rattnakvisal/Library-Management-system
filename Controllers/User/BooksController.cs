9138abfcb3d1375bea2301a376cb0d03bd42713e
ShuZhen168 <sokchen7080r@gmail.com>
2026-02-07 11:06:26 +0700

    detail

--- Controllers/User/BooksController.cs
+++ Controllers/User/BooksController.cs
@@ -1,6 +1,13 @@
-﻿namespace Library_Management_system.Controllers.user
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Library_Management_system.Controllers.user
 {
-    public class BooksController
+    public class BooksController : Controller
     {
+        [Route("user/books")]
+        public IActionResult BookDetail()
+        {
+            //return View();
+            return View("~/Views/User/Books/BookDetail.cshtml");
+        }
     }
 }
